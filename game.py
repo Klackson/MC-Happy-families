pimc + bugfix + writefile + softmax

game.py
@@ -119,7 +119,7 @@ class game:
         if self.verbose : print("Player", player_number, "asks player", asked_player, "for card", asked_card, "in family", asked_family)
 
         # Naive assumption that the player is not bluffing and doesn't own the card (or probably doesn't)
-        self.beliefs[player_number, asked_family, asked_card] = 0.1
+        self.beliefs[player_number, asked_family, asked_card] = max(0.1, self.beliefs[player_number, asked_family, asked_card] - 0.7)
 
         # Update belief about families held in hand by player
         unseen_asked_family_cards = [ person for person in range(self.nb_people_per_family) if self.card_tracker[asked_family, person] == -1 #]
@@ -159,12 +159,17 @@ class game:
     
 
     def play_turn(self, player_number):
+        if len(self.hands[player_number]) == 0:
+                return #Player has no cards, he can't play
+        
         if self.verbose : print("Player", player_number, "playing")
 
-        self.converge_beliefs() # Might wanna do it inside lucky loop
+        
 
         lucky = True
         while lucky:
+            self.converge_beliefs() # Might wanna do it inside lucky loop
+            
             if self.player_list[player_number].capitalize() == "Human":
                 lucky = self.ask(player_number)
             
@@ -179,14 +184,22 @@ class game:
             elif self.player_list[player_number].capitalize() == "Smarterai":
                 choice = nestedai.better_choose_move(self, player_number, verbose = self.verbose)
                 lucky = self.ask(player_number, choice)
+            
+            elif self.player_list[player_number].capitalize() == "Pimc":
+                choice = simpleai.choose_move_pimc(self, player_number, verbose = self.verbose)
+                lucky = self.ask(player_number, choice)
+
+            elif self.player_list[player_number].capitalize() == "Nested_pimc":
+                choice = nestedai.choose_move_pimc(self, player_number, verbose = self.verbose)
+                lucky = self.ask(player_number, choice)
 
             else:
                 raise ValueError("Invalid player type")
-
+            
             self.is_family_scored()
+            if not len(self.hands[player_number]) or self.is_game_over():
+                return #Player has no cards, he can't play or game over
 
-            if len(self.hands[player_number]) == 0:
-                return #Player has no cards, he can't play
 
             if lucky and self.verbose : print("Player", player_number, "got lucky and can play again\n")
 
@@ -282,20 +295,24 @@ class game:
 
 
     def deal_other_hands_v2(self, player_number, deck):
-        newhands = [[] for _ in range(len(self.hands))]
+        newhands = [[] for _ in range(self.nb_players)]
 
         # First add known cards to hands
         for family in range(self.nb_families):
             for person in range(self.nb_people_per_family):
                 owner = self.card_tracker[family, person]
-                if owner >= 0:
+                if owner > -1 and owner != player_number:
                     newhands[owner].append([family, person])
+
+        newhands[player_number] = self.hands[player_number].copy()
 
         for player, hand in enumerate(self.hands):
             if player == player_number:
-                newhands[player_number] = hand.copy()
                 continue
-            
+                
+            if not len(deck): 
+                break
+
             candidate_cards = []
             candidate_weights = []
             for card in deck :
@@ -307,14 +324,14 @@ class game:
 
             p = candidate_weights/np.sum(candidate_weights)
 
-            if p.sum() - 1> 0.001 or p.sum() - 1 < -0.001:
+            if abs(p.sum() - 1)> 0.001 :
                 print("P :",p)
                 print("Sum :", p.sum())
                 raise ValueError("problem non 1 probability sum")
 
             added_cards_indices = np.random.choice(len(candidate_cards), remanining_cards_to_add, p = candidate_weights/np.sum(candidate_weights), replace = False)
-            added_cards = [candidate_cards[i] for i in added_cards_indices]
-            for card in added_cards:
+            for i in added_cards_indices:
+                card = candidate_cards[i]
                 newhands[player].append(card)
                 deck.remove(card)
         
@@ -322,7 +339,7 @@ class game:
 
     def play_game(self):
         turn = 0
-        max_turns = 10e6
+        max_turns = 10e5
         while turn < max_turns:
             player = turn % self.nb_players
             self.play_turn(player)
@@ -330,12 +347,14 @@ class game:
             
             if self.is_game_over() :
                 scores = self.compute_scores()
-                if self.verbose : print("Game over! Scores :", scores)
+                if self.verbose : 
+                    print("Game over! Scores :", scores)
+                    print("Final hands :", self.hands)
 
                 return scores
                 
         if self.verbose : print('Game over because too long')
-        return -1
+        raise ValueError("Game over because too long")
     
     def converge_beliefs(self):
         self.beliefs += 0.1 * (self.beliefs.mean() - self.beliefs)

nestedai.py
@@ -1,7 +1,7 @@
 import game, montecarlo, numpy as np, time, copy, simpleai
 
 params={"nb_total_simulations":30,
-        "nb_worlds":30
+        "nb_worlds":6
         }
 
 params["nb_nested_simulations"] = params["nb_total_simulations"] // params["nb_worlds"]
@@ -131,8 +131,82 @@ def best_move_value_v2(instance , player_number, return_opponent = False):
                 search_data[i,j] = simulation.playout(player_number, lucky)[player_number] 
 
     mean_scores = np.mean(search_data, axis=0)
+    soft_scores = softmax(mean_scores, 2)
 
     if return_opponent:
-        return np.min(mean_scores)
+        return np.min(soft_scores)
 
-    return np.max(mean_scores) # Simplified UCB
+    return np.max(soft_scores) # Simplified UCB
+
+
+def softmax(array, temperature):
+    heated_array = array / temperature
+    return np.exp(heated_array) / np.sum(np.exp(heated_array))
+
+
+
+def choose_move_pimc(instance , player_number, verbose = False):
+    starttime = time.time()
+
+    moves = simpleai.enumerate_moves(instance, player_number)
+
+    search_data = np.empty((params["nb_worlds"], len(moves)))
+
+    for i in range(params["nb_worlds"]):
+        assumed_hands, assumed_pile = instance.assume_game_state_v2(player_number)
+
+        for j, move in enumerate(moves):
+            re_instance = copy.deepcopy(instance)
+            re_instance.hands = copy.deepcopy(assumed_hands)
+            re_instance.pile = assumed_pile.copy()
+            re_instance.verbose = False
+            re_instance.is_family_scored()
+            lucky = re_instance.ask(player_number, chosen = move)
+
+            re_instance.is_family_scored()
+
+            if len(re_instance.hands[player_number]) == 0: lucky = False
+
+            if re_instance.is_game_over():
+                search_data[i,j] = re_instance.compute_scores()[player_number]
+                continue
+
+            if lucky :
+                search_data[i,j] = best_move_value_pimc(re_instance, player_number)
+                continue
+
+            search_data[i,j] = best_move_value_pimc(re_instance, 1-player_number, True) # Only retrieves score for the player for now
+
+    if verbose :
+        print("Evaluated",len(moves),"moves in", time.time() - starttime, "seconds")
+
+    mean_scores = np.mean(search_data, axis=0)
+
+    return moves[np.argmax(mean_scores)]
+
+
+def best_move_value_pimc(instance , player_number, return_opponent = False):
+    moves = simpleai.enumerate_moves(instance, player_number)
+
+    search_data = np.empty((params["nb_nested_simulations"], len(moves)))
+
+    for i in range(params["nb_nested_simulations"]):
+        assumed_hands, assumed_pile = instance.assume_game_state_v2(player_number)
+        
+        for j, move in enumerate(moves):
+            simulation = montecarlo.simulation(copy.deepcopy(assumed_hands), copy.deepcopy(assumed_pile), instance.families_scored.copy())
+            lucky = simulation.ask_chosen(player_number, move)
+            playing_player = (player_number + (not lucky)) % instance.nb_players
+            
+            if return_opponent:
+                search_data[i,j] = simulation.pimc(playing_player)[1-player_number]
+            else:
+                search_data[i,j] = simulation.pimc(playing_player)[player_number] 
+
+    mean_scores = np.mean(search_data, axis=0)
+    soft_scores = softmax(mean_scores, 2)
+
+    if return_opponent:
+        return np.min(soft_scores)
+
+    return np.max(soft_scores) # Simplified UCB

simpleai.py
@@ -25,23 +25,60 @@ def enumerate_moves(instance, player_number):
 
 
 def choose_move(instance , player_number, verbose = True):
-    starttime = time.time()
-
     moves = enumerate_moves(instance, player_number)
 
     search_data = np.empty((params["nb_simulations"], len(moves)))
 
     for i in range(params["nb_simulations"]):
-        assumed_hands, assumed_pile = instance.assume_game_state(player_number)
+        assumed_hands, assumed_pile = instance.assume_game_state_v2(player_number)
         
         for j, move in enumerate(moves):
-            simulation = montecarlo.simulation(copy.deepcopy(assumed_hands), copy.deepcopy(assumed_pile), instance.families_scored.copy())
+            try :
+                simulation = montecarlo.simulation(copy.deepcopy(assumed_hands), copy.deepcopy(assumed_pile), instance.families_scored.copy())
+            except:
+                print("Error in simulation")
+                print("True hands :",instance.hands)
+                print("True pile :", instance.pile)
+                print("--------------------")
+                print("Assumed hands :", assumed_hands)
+                print("Assumed pile :", assumed_pile)
+
             lucky = simulation.ask_chosen(player_number, move)
 
             search_data[i,j] = simulation.playout(player_number, lucky)[player_number] # Only retrieves score for the player for now
 
     mean_scores = np.mean(search_data, axis=0)
 
-    if verbose : print("Runtime :", time.time() - starttime)
+
+    return moves[np.argmax(mean_scores)] # Simplified UCB
+
+
+def choose_move_pimc(instance , player_number, verbose = True):
+    moves = enumerate_moves(instance, player_number)
+
+    search_data = np.empty((params["nb_simulations"], len(moves)))
+
+    for i in range(params["nb_simulations"]):
+        assumed_hands, assumed_pile = instance.assume_game_state_v2(player_number)
+        
+        for j, move in enumerate(moves):
+            try :
+                simulation = montecarlo.simulation(copy.deepcopy(assumed_hands), copy.deepcopy(assumed_pile), instance.families_scored.copy())
+            except:
+                print("Player number :", player_number)
+                print("True hands :",instance.hands)
+                print("True pile :", instance.pile)
+                print("--------------------")
+                print("Assumed hands :", assumed_hands)
+                print("Assumed pile :", assumed_pile)
+                raise ValueError("Error in simulation")
+            
+            lucky = simulation.ask_chosen(player_number, move)
+
+            playing_player = (player_number + (not lucky)) % instance.nb_players
+
+            search_data[i,j] = simulation.pimc(playing_player)[player_number] # Only retrieves score for the player for now
+
+    mean_scores = np.mean(search_data, axis=0)
 
     return moves[np.argmax(mean_scores)] # Simplified UCB